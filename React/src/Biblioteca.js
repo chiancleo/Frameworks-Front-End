a2da2196c77ee5f6c86b505860c351f1ac4fce56
chiancleo <chiancldl@hotmail.com>
2020-09-20 22:01:54 -0300

    Inserindo imagens e ajustando estilos

--- React/src/Biblioteca.js
+++ React/src/Biblioteca.js
@@ -1,31 +1,35 @@
 import React from 'react'
 import Livro from './Livro.js'
+import viagem from './viagem.png'
+import casmurro from './casmurro.jpg'
+import senhora from './senhora.jpg'
+import horto from './horto.jpg'
 
 export default class Biblioteca extends React.Component {
   livros = [
     {
       statement: 'Dom Casmurro',
       autor: 'Machado de Assis',
-      descricao: 'Aqui conterá uma descrição da obra',
-      imagem: './viagem.png'
+      descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sodales felis nec lacinia dapibus. Praesent tempus turpis ipsum, sit amet iaculis nisi tempor sed. Donec sit amet justo eu dolor varius consequat sit amet non felis. Cras luctus convallis blandit. Donec eget iaculis ipsum. Phasellus ligula neque, convallis in dictum ac, cursus non neque. Integer sit amet convallis turpis, in fringilla tortor. Curabitur interdum neque sed volutpat vestibulum. Aliquam erat volutpat. Nulla sit amet pulvinar orci, at pellentesque dolor. Proin fringilla id felis sed egestas. Aliquam molestie sit amet orci ac consequat.',
+      imagem: casmurro
     },
     {
       statement: 'Senhora',
       autor: 'José de Alencar',
-      descricao: 'Aqui conterá uma descrição da obra',
-      imagem: './viagem.png'
+      descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sodales felis nec lacinia dapibus. Praesent tempus turpis ipsum, sit amet iaculis nisi tempor sed. Donec sit amet justo eu dolor varius consequat sit amet non felis. Cras luctus convallis blandit. Donec eget iaculis ipsum. Phasellus ligula neque, convallis in dictum ac, cursus non neque. Integer sit amet convallis turpis, in fringilla tortor. Curabitur interdum neque sed volutpat vestibulum. Aliquam erat volutpat. Nulla sit amet pulvinar orci, at pellentesque dolor. Proin fringilla id felis sed egestas. Aliquam molestie sit amet orci ac consequat.',
+      imagem: senhora
     },
     {
       statement: 'Horto',
       autor: 'Auta de Souza',
-      descricao: 'Aqui conterá uma descrição da obra',
-      imagem:'./viagem.png'
+      descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sodales felis nec lacinia dapibus. Praesent tempus turpis ipsum, sit amet iaculis nisi tempor sed. Donec sit amet justo eu dolor varius consequat sit amet non felis. Cras luctus convallis blandit. Donec eget iaculis ipsum. Phasellus ligula neque, convallis in dictum ac, cursus non neque. Integer sit amet convallis turpis, in fringilla tortor. Curabitur interdum neque sed volutpat vestibulum. Aliquam erat volutpat. Nulla sit amet pulvinar orci, at pellentesque dolor. Proin fringilla id felis sed egestas. Aliquam molestie sit amet orci ac consequat.',
+      imagem:horto
     },
     {
       statement: 'Viagem ao céu',
       autor: 'Monteiro Lobato',
-      descricao: 'Aqui conterá uma descrição da obra',
-      imagem:'./viagem.png'
+      descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sodales felis nec lacinia dapibus. Praesent tempus turpis ipsum, sit amet iaculis nisi tempor sed. Donec sit amet justo eu dolor varius consequat sit amet non felis. Cras luctus convallis blandit. Donec eget iaculis ipsum. Phasellus ligula neque, convallis in dictum ac, cursus non neque. Integer sit amet convallis turpis, in fringilla tortor. Curabitur interdum neque sed volutpat vestibulum. Aliquam erat volutpat. Nulla sit amet pulvinar orci, at pellentesque dolor. Proin fringilla id felis sed egestas. Aliquam molestie sit amet orci ac consequat.',
+      imagem:viagem
     }
   ]
 
@@ -45,6 +49,7 @@ export default class Biblioteca extends React.Component {
           statement={this.livros[this.state.current].statement}
           autor={this.livros[this.state.current].autor}
           descricao = {this.livros[this.state.current].descricao}
+          imagem = {this.livros[this.state.current].imagem}
         />
       </div>
       )
--- React/src/Livro.js
+++ React/src/Livro.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import viagem from './viagem.png'
 
 export default class Livro extends React.Component {
   render() { 
     return (
       <div>
-        <img src={this.props.imagem} alt="Falha ao carregar" width={60} height={40}/>
-        <h1>{this.props.statement}</h1>
-        <h3>{this.props.autor}</h3>
-        <p>{this.props.descricao}</p>
+        <img src={this.props.imagem} alt="Falha ao carregar" width={100} height={140}/>
+        <h1>Livro: {this.props.statement}</h1>
+        <h2>Autor: {this.props.autor}</h2>
+        <h4>Descrição:</h4>
+        <p className="paragrafo">{this.props.descricao}</p>
       </div>
     )
   }
