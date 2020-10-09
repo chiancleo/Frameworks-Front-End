87affefba8eb5f343f760477765a422d1e90e9da
chiancleo <45492281+chiancleo@users.noreply.github.com>
2020-10-09 16:28:43 -0300

    Adicionando componentes em svelte

---
+++ Svelte/App.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Biblioteca from "./Biblioteca.svelte";
+</script>
+
+<style>
+</style>
+
+<main>
+  <Biblioteca />
+</main>
---
+++ Svelte/Livro.svelte
@@ -0,0 +1,11 @@
+<h1>{ statement }</h1>
+<h2>{ autor }</h2>
+<h4>{ header }</h4>
+<p>{ descricao }</p>
+
+<script>
+  let header = "Descrição";
+  export let statement;
+  export let autor;
+  export let descricao;
+</script>
